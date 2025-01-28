6cbd578e4fba88511fa5a13020d6d298aa9988d8
Eli Butters <elibuttersover@gmail.com>
2025-01-28 00:00:29 -0500

    got rid of venv

--- Site/client/src/dash_presets/ChartMap.js
+++
@@ -1,9 +0,0 @@
-import ExRHC from "../components/ChartTypes.js/ExRHC"
-
-const CHART_MAP = {
-    'Price Chart': ExRHC,
-    'Spot Chart': ExRHC,
-    'Order Book': ExRHC,
-}
-
-export default CHART_MAP;
